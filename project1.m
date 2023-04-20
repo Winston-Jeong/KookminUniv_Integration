395a7f16b77d97202ad3b9ecf619a4f06058d057
Hyeonwoo Jeong <130826299+Winston-Jeong@users.noreply.github.com>
2023-04-20 17:31:34 +0900

    Update project1.m

--- project1.m
+++ project1.m
@@ -8,4 +8,64 @@ for i = 1:num_vectors
     z = input('z 좌표를 입력하세요: ');
     vectors(i, :) = [x, y, z];
 end
-%fprintf('%d ', vectors(3, :))
+%%2. 유클리디안 거리, 코사인 유사도
+%유클리디안거리
+dista = norm(vectors(1, :)-vectors(2, :));
+distb = norm(vectors(2, :)-vectors(3, :));
+distc = norm(vectors(3, :)-vectors(1, :));
+%코사인유사도
+cosa = cosineSimilarity(vectors(1, :), vectors(2, :));
+cosb = cosineSimilarity(vectors(2, :), vectors(3, :));
+cosc = cosineSimilarity(vectors(3, :), vectors(1, :));
+
+%유클리디안거리
+if(dista<=distb) 
+    if(dista<=distc)
+        nearvec=disda;
+    end
+    if(dista>distc)
+        nearvec=distc;
+    end
+end
+
+if(dista>distb) 
+    if(distb<=distc)
+        nearvec=distb;
+    end
+    if(distb>distc)
+        nearvec=distc;
+    end
+end
+
+if(nearvec==dista) 
+    near_v1=vectors(1, :);
+    near_v2=vectors(2, :);
+end
+
+if(nearvec==distb) 
+    near_v1=vectors(2, :); 
+    near_v2=vectors(3, :);
+end
+
+if(nearvec==distc) 
+    near_v1=vectors(3, :);
+    near_v2=vectors(1, :);
+end
+%코사인유사도
+if(cosa>=cosb) 
+    if(cosa>=cosc)
+        samevec=cosa;
+    end
+    if(cosa<cosc)
+        samevec=cosc;
+    end
+end
+
+if(cosa<cosb) 
+    if(cosb>=cosc)
+        samevec=cosb;
+    end
+    if(cosb<cosc)
+        samevec=cosc;
+    end
+end
