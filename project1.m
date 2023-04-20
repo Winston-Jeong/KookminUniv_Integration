cb298c3b4f283d39e1cfbf5e6c5e7291306566b8
Hyeonwoo Jeong <130826299+Winston-Jeong@users.noreply.github.com>
2023-04-20 17:23:04 +0900

    Add files via upload

--- project1.m
+++ project1.m
@@ -1,11 +1,11 @@
-%%2. 유클리디안 거리, 코사인 유사도
+%%1. 백터 입력
+num_vectors = 3;
+vectors = zeros(num_vectors, 3);
 
-%유클리디안 거리
-dista = norm(vectors(1, :)-vectors(2, :));
-distb = norm(vectors(2, :)-vectors(3, :));
-distc = norm(vectors(3, :)-vectors(1, :));
-
-%코사인유사도
-cosa = cosineSimilarity(vectors(1, :), vectors(2, :));
-cosb = cosineSimilarity(vectors(2, :), vectors(3, :));
-cosc = cosineSimilarity(vectors(3, :), vectors(1, :));
+for i = 1:num_vectors
+    x = input('x 좌표를 입력하세요: ');
+    y = input('y 좌표를 입력하세요: ');
+    z = input('z 좌표를 입력하세요: ');
+    vectors(i, :) = [x, y, z];
+end
+%fprintf('%d ', vectors(3, :))
