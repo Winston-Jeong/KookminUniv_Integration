06211d3bab82d734de4f2c9f3bcf9ebca5c938cb
Hyeonwoo Jeong <130826299+Winston-Jeong@users.noreply.github.com>
2023-04-20 17:47:39 +0900

    Update project1.m

--- project1.m
+++ project1.m
@@ -8,4 +8,56 @@ for i = 1:num_vectors
     z = input('z 좌표를 입력하세요: ');
     vectors(i, :) = [x, y, z];
 end
-%fprintf('%d ', vectors(3, :))
+
+%%2. ~~유클리디안, 코사인유사도 계산 후
+%유클리디안거리 작은 값 찾기
+if(dista<=distb) 
+    if(dista<=distc)
+        nearvec=dista;
+    end
+    if(dista>distc)
+        nearvec=distc;
+    end
+end
+
+if(dista>distb) 
+    if(distb<=distc)
+        nearvec=distb;
+    end
+    if(distb>distc)
+        nearvec=distc;
+    end
+end
+
+if(nearvec==dista) 
+    near_v1=vectors(1, :);
+    near_v2=vectors(2, :);
+end
+
+if(nearvec==distb) 
+    near_v1=vectors(2, :); 
+    near_v2=vectors(3, :);
+end
+
+if(nearvec==distc) 
+    near_v1=vectors(3, :);
+    near_v2=vectors(1, :);
+end
+%코사인유사도 큰 값 찾기
+if(cosa>=cosb) 
+    if(cosa>=cosc)
+        samevec=cosa;
+    end
+    if(cosa<cosc)
+        samevec=cosc;
+    end
+end
+
+if(cosa<cosb) 
+    if(cosb>=cosc)
+        samevec=cosb;
+    end
+    if(cosb<cosc)
+        samevec=cosc;
+    end
+end
